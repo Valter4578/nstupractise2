eea07ab3a0b229a70881e38257fcc888e243cf20
Maksim Alekseev <mrvalter566@gmail.com>
2024-09-30 15:22:19 +0700

    init commit

---
+++ Practise2(task1)/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array_num = new int[n];
+            for (int i = 0; i < n; i++) array_num[i] = i + 1;
+            
+            for (int j = n-1; j >= 0; j--)
+            {
+                Console.WriteLine(Convert.ToString(array_num[j]));
+            }
+            
+
+            int matrix_size = Convert.ToInt32(Console.ReadLine());
+            int[,] squareArray = new int[matrix_size, matrix_size];
+            for (int i = 0; i < matrix_size; i++)
+                for (int j = 0; j < matrix_size; j++)
+                    squareArray[i, j] = (i - 1 <= j) ? 1 : 0;
+
+            for (int i = 0; i < matrix_size; i++)
+            {
+                for (int j = 0; j < matrix_size; j++)
+                    Console.Write(squareArray[i, j] + " ");
+                Console.WriteLine();
+            }
+
+            int M = Convert.ToInt32(Console.ReadLine());
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int[M, N];
+            int value = 1;
+
+            int top = 0, bottom = M - 1, left = 0, right = N - 1;
+            while (value <= M * N)
+            {
+                for (int i = left; i <= right; i++)
+                {
+                    array[top, i] = value++;
+                }
+                top++;
+
+                for (int i = top; i <= bottom; i++)
+                {
+                    array[i, right] = value++;
+                }
+                right--;
+
+                if (top <= bottom)
+                {
+                    for (int i = right; i >= left; i--)
+                    {
+                        array[bottom, i] = value++;
+                    }
+                    bottom--;
+                }
+
+                if (left <= right)
+                {
+                    for (int i = bottom; i >= top; i--)
+                    {
+                        array[i, left] = value++;
+                    }
+                    left++;
+                }
+            }
+
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Practise2(task2)/ConsoleApp1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+// Перечисление для типов операций
+enum OperationType
+{
+    Addition,       // Сложение
+    Subtraction,    // Вычитание
+    Multiplication, // Умножение
+    Division,       // Деление
+    Power,          // Возведение в степень
+    SquareRoot      // Вычисление квадратного корня
+}
+
+class Program
+{
+    // Функция для выполнения операции
+    static double DoOperation(OperationType op, double a, double b = 0)
+    {
+        switch (op)
+        {
+            case OperationType.Addition:
+                return a + b;
+            case OperationType.Subtraction:
+                return a - b;
+            case OperationType.Multiplication:
+                return a * b;
+            case OperationType.Division:
+                if (b != 0)
+                {
+                    return a / b;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: деление на ноль!");
+                    return 0;
+                }
+            case OperationType.Power:
+                return Math.Pow(a, b);
+            case OperationType.SquareRoot:
+                if (a >= 0)
+                {
+                    return Math.Sqrt(a);
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: квадратный корень из отрицательного числа!");
+                    return 0;
+                }
+            default:
+                Console.WriteLine("Ошибка: неизвестная операция!");
+                return 0;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Выберите операцию:");
+        Console.WriteLine("1. Сложение");
+        Console.WriteLine("2. Вычитание");
+        Console.WriteLine("3. Умножение");
+        Console.WriteLine("4. Деление");
+        Console.WriteLine("5. Возведение в степень");
+        Console.WriteLine("6. Вычисление квадратного корня");
+
+        int choice = int.Parse(Console.ReadLine());
+        OperationType operation = (OperationType)(choice - 1);
+
+        double a, b;
+
+        if (operation == OperationType.SquareRoot)
+        {
+            Console.WriteLine("Введите число для вычисления квадратного корня:");
+            a = double.Parse(Console.ReadLine());
+            b = 0;
+        }
+        else
+        {
+            Console.WriteLine("Введите первое число:");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            b = double.Parse(Console.ReadLine());
+        }
+
+        double result = DoOperation(operation, a, b);
+        Console.WriteLine("Результат: " + result);
+    }
+}
